a91190af5d87210f2b77314fdd79623cefcc3c02
eurodengist <mikolaus54@proton.me>
2025-09-29 20:50:25 +0000

    Add files via upload

---
+++ ideologies.js
@@ -0,0 +1,122 @@
+const ideologies = { // eslint-disable-line no-unused-vars
+  'Orthodox-Marxism': {
+    a: 65,
+    b: 35,
+    c: 60,
+    d: 85,
+    e: 65,
+    f: 40
+  },
+  'Leninism': {
+    a: 90,
+    b: 25,
+    c: 90,
+    d: 80,
+    e: 90,
+    f: 70
+  },
+  'Marxism-Leninism': {
+    a: 90,
+    b: 15,
+    c: 100,
+    d: 30,
+    e: 100,
+    f: 100
+  },
+  'Trotskyism': {
+    a: 100,
+    b: 40,
+    c: 80,
+    d: 100,
+    e: 90,
+    f: 80
+  },
+  'Frankfurt School': {
+    a: 15,
+    b: 85,
+    c: 40,
+    d: 70,
+    e: 10,
+    f: 10
+  },
+  'Council-Communism': {
+    a: 100,
+    b: 60,
+    c: 20,
+    d: 90,
+    e: 10,
+    f: 70
+  },
+  'Maoism': {
+    a: 100,
+    b: 10,
+    c: 80,
+    d: 40,
+    e: 90,
+    f: 20
+  },
+  'Anarcho-Communism': {
+    a: 100,
+    b: 80,
+    c: 0,
+    d: 100,
+    e: 0,
+    f: 40
+  },
+  'Luxemburgism': {
+    a: 90,
+    b: 50,
+    c: 35,
+    d: 90,
+    e: 50,
+    f: 30
+  },
+  'Titoism': {
+    a: 50,
+    b: 40,
+    c: 50,
+    d: 20,
+    e: 70,
+    f: 40
+  },
+  'Hoxhaism': {
+    a: 100,
+    b: 30,
+    c: 100,
+    d: 50,
+    e: 100,
+    f: 90
+  },
+  'Eurocommunism': {
+    a: 10,
+    b: 60,
+    c: 40,
+    d: 25,
+    e: 60,
+    f: 30
+  },
+  'Sankarism': {
+    a: 70,
+    b: 20,
+    c: 70,
+    d: 40,
+    e: 50,
+    f: 15
+  },
+  'Juche': {
+    a: 80,
+    b: 0,
+    c: 100,
+    d: 0,
+    e: 100,
+    f: 50
+  },
+  'Bordigism': {
+    a: 100,
+    b: 50,
+    c: 100,
+    d: 100,
+    e: 100,
+    f: 90
+  }
+}
